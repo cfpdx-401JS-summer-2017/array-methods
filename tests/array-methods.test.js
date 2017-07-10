cdc7b464bb6aeb7090e5d7aaa2b2ddbb7a1ede30
christylaguardia <christinelaguardia@gmail.com>
2017-07-10 16:15:24 -0700

    added map method test

--- tests/array-methods.test.js
+++ tests/array-methods.test.js
@@ -8,4 +8,11 @@ describe('array library', () => {
         assert.equal(length, 4);
         assert.equal(array[3], 4);
     });
+
+    it('maps the array and return lengths', () => {
+        const array = [1, 2, 3];
+        const newArray = array.map(function(x){ return x + 1; });
+        assert.equal(array.length, newArray.length);
+    });
+
 });
