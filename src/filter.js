d58ebb94801cba816170213c1cfbce239aa78c53
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-11 10:48:13 -0700

    simplify filter.js

--- src/filter.js
+++ src/filter.js
@@ -1,10 +1,8 @@
 function filter(array, callback) {
     const newArray = [];
-    let counter = 0;
-    for (let i=0; i<array.length; i++) {
-        if(callback(array[i])) {
-            newArray[counter] = array[i];
-            counter++;
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            newArray[newArray.length] = array[i];
         }
     }
     return newArray;
