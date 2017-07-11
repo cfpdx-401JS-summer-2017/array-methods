bb65005af8e85d6c7438459c4df59c3e644c2fb0
christylaguardia <christinelaguardia@gmail.com>
2017-07-10 17:13:54 -0700

    added filter method test

--- src/array-methods.js
+++ src/array-methods.js
@@ -1,8 +1,32 @@
 const methods = {};
-
+// push
 methods.push = function(array, item) {
     array.push(item);
     return array.length;
+};
+// for each
+methods.foreach = function(array) {
+    const numbers = [];
+    array.forEach(function(x) {
+        if(Number.isInteger(x)) {
+            numbers.push(x);
+        }
+    });
+    return numbers.length;
+};
+// map
+methods.map = function(array) {
+    const newArray = array.map(function(x){
+        return x + 1; }
+    );
+    return newArray.length;
+};
+// filter
+methods.filter = function(array) {
+    const filteredArray = array.filter(function(item) {
+        return Number.isInteger(item);
+    });
+    return filteredArray.length;
 };
 
 module.exports = methods;
--- tests/array-methods.test.js
+++ tests/array-methods.test.js
@@ -12,18 +12,20 @@ describe('array library', () => {
     // for each
     it('loops through the array and counts numbers', () => {
         const array = [1, 'a', 2, 'b', 3, 'c'];
-        const numbers = [];
-        array.forEach(function(x) {
-            if(Number.isInteger(x)) { numbers.push(x); }
-        });
-        assert.equal(numbers.length, 3);
+        const length = methods.foreach(array);
+        assert.equal(length, 3);
     });
     // map
-    it('maps the array and return lengths', () => {
+    it('maps the array to a new array and returns the new length', () => {
         const array = [1, 2, 3];
-        const newArray = array.map(function(x){ return x + 1; });
-        assert.equal(array.length, newArray.length);
+        const length = methods.map(array);
+        assert.equal(length, 3);
     });
-
-
+    // filter
+    it('filters the array for numbers and returns the new length', () => {
+        const array = [1, 'a', 2, 'b'];
+        const index = methods.filter(array);
+        assert.equal(index, 2);
+    });
+    
 });
