9c3278bd34cc6af2f9cac9645d8ea1abd4d378ba
Joe Churchman <churchman.joe@gmail.com>
2017-07-10 17:40:50 -0700

    passed map test

--- src/array-methods.js
+++ src/array-methods.js
@@ -1,18 +1,17 @@
 const arrayObj = {};
 
-arrayObj.forEachLoop = (arr, callback) => {
+arrayObj.forEach = (arr, callback) => {
   for(let i=0; i < arr.length; i++){
     callback(arr[i], i);
   }
 };
 
-arrayObj.mapArray = (arr, callback) => {
-  for (let i = 0; i < arr.lenght; i++) {
-    callback(arr[i], i);
-    const newArray = [];
-    newArray[i] = arr[i];
-    return newArray;
+arrayObj.map = (arr, callback) => {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray[i] = callback(arr[i], i);
   }
+  return newArray;
 };
 
 
--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,13 +1,11 @@
 const assert = require('assert');
 const arrayObj = require('../src/array-methods');
 
-
-
 describe('array methods', () => {
   it ('does something to each item in an array', () => {
     const testArray = [1,2,3];
     const results = [];
-    const forEachLooped = arrayObj.forEachLoop(testArray, (item, index) => {
+    arrayObj.forEach(testArray, (item, index) => {
       results.push({
         item,
         index
@@ -16,17 +14,15 @@ describe('array methods', () => {
     assert.deepEqual(results, [
       {index: 0, item: 1},
       {index: 1, item: 2},
-      {index: 2, item: 3},
-    
+      {index: 2, item: 3}
     ]);
   });
 
   it ('does something to each item in an array and returns a new array', () => {
-    const testArray = [1,2,3];
-    const mappedArray = arrayObj.mapArray(testArray, () => {
-;
-    });
-    assert.equal(mappedArray, [2,3,4]);
+    const testArray = [4,8,13];
+    const mappedArray = arrayObj.map(testArray, (item, index) => item + 3
+    );
+    assert.deepEqual(mappedArray, [7,11,16]);
   });
 
 
