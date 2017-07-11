bc2e33b547c5a6bf27edbafbeda9917dd3d8be8a
Meryl Turner <merylturner08@gmail.com>
2017-07-10 22:27:07 -0700

    all tests have passed

--- src/reduce.js
+++ src/reduce.js
@@ -1,7 +1,6 @@
 function myReduce(array,callback) {
     const newArray = [];
     for (let i = 0; i < array.length; i++) {
-        // initialValue = 0;
         newArray[i] = callback(array[i]);
     }
     return newArray;
