a2815973766bb4dc45b0196e5c18fc7c1fe20a95
Christopher Thomson <christopher.thomson42@gmail.com>
2017-07-10 21:55:09 -0700

    map attempt

--- src/arravy.js
+++ src/arravy.js
@@ -2,7 +2,46 @@
 function push(array, item) {
   array[array.length] = item;
   return array.length;
-}
+};
+
+function forEach(array, callback) {
+  for (i = 0; i < array.length; i++) {
+      callback(array[i], i);
+
+  }
+};
+console.log(forEach());
+
+function map(array, callback) {
+  for (i = 0; i < array.length; i++) {
+    callback(array)
+  }
+  array[array.length] = item;
+
+  return ;
+};
+// function filter(array, callback) {
+//   array[array.length] = item;
+//   return array.length;
+// };
+
+// function reduce(array, callback [, initialValue]) {
+//   array[array.length] = item;
+//   return array.length;
+// };
+
+// function findIndex(array, callback) {
+//   array[array.length] = item;
+//   return array.length;
+// };
+
+// function every(array, callback) {
+//   array[array.length] = item;
+//   return array.length;
+// };
+
 module.exports = {
-    push 
+    push, 
+    forEach,
+    map
 };
--- tests/tester.js
+++ tests/tester.js
@@ -10,3 +10,61 @@ describe('push', () =>  {
         assert.equal(array[3], 4);
     });
 });
+
+describe('forEach', () =>  {
+    it('iterates though each item in an array', () => {
+        const array = [1, 2, 3];
+        const fill = [];
+        arravy.forEach(array, function(int,index) {
+            fill[index]= int
+
+        });
+    
+        assert.deepEqual(fill,[1,2,3]);
+    });
+});
+
+describe('map(array, callback)', () =>  {
+    it('iterates though an array and builds off of it forming a new one', () => {
+        const array = [1, 2, 3];
+        const length = arravy.push(array, 4);
+        assert.equal(length, 4);
+        assert.equal(array[3], 4);
+    });
+});
+
+// describe('filter(array, callback)', () =>  {
+//     it('adds and returns new length', () => {
+//         const array = [1, 2, 3];
+//         const length = arravy.push(array, 4);
+//         assert.equal(length, 4);
+//         assert.equal(array[3], 4);
+//     });
+// });
+
+// describe('reduce(array, callback [, initialValue])', () =>  {
+//     it('adds and returns new length', () => {
+//         const array = [2, 0, 3];
+//         const length = arravy.push(array, 4);
+//         assert.equal(length, 4);
+//         assert.equal(array[3], 4);
+//     });
+// });
+
+// describe('findIndex(array, callback)', () =>  {
+//     it('adds and returns new length', () => {
+//         const array = [1, 2, 3];
+//         const length = arravy.push(array, 4);
+//         assert.equal(length, 4);
+//         assert.equal(array[3], 4);
+//     });
+// });
+
+// describe('every(array, callback)', () =>  {
+//     it('adds and returns new length', () => {
+//         const array = [1, 2, 3];
+//         const length = arravy.push(array, 4);
+//         assert.equal(length, 4);
+//         assert.equal(array[3], 4);
+//     });
+// });
