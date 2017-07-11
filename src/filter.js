424abd1785e033d9e0167ae1354bace25b4b8c25
Meryl Turner <merylturner08@gmail.com>
2017-07-10 18:26:44 -0700

    created filter js and passed test

---
+++ src/filter.js
@@ -0,0 +1,9 @@
+function myFilter(array,callback) {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        //filter array if it passes test?
+    }
+    return newArray;
+}
+
+module.exports = myFilter;
--- tests/map.test.js
+++ tests/map.test.js
@@ -4,7 +4,7 @@ const myMap = require('../src/map');
 describe('creates new array with return value of callback', () => {
     it('creates new array with return value of callback', () => {
         const testArray = [1, 2, 3];
-        // const newArray = [];
+        
         function testCallback(item, index) {
             let mappedValue = item += 1;
             return mappedValue;
