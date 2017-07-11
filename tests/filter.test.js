baac423e5e180e61861f4b301b4bc1b489a235f9
Meryl Turner <merylturner08@gmail.com>
2017-07-10 18:31:54 -0700

    removed unused parameters

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -5,7 +5,7 @@ describe('creates new array from items that pass test in the callback', () => {
     it('creates new array from items that pass test in the callback', () => {
         const testArray = [1,2,3];
 
-        function testCallback(item, index) {
+        function testCallback(item) {
             return item >= 0;
         }
 
--- tests/map.test.js
+++ tests/map.test.js
@@ -5,7 +5,7 @@ describe('creates new array with return value of callback', () => {
     it('creates new array with return value of callback', () => {
         const testArray = [1, 2, 3];
 
-        function testCallback(item, index) {
+        function testCallback(item) {
             let mappedValue = item += 1;
             return mappedValue;
         }
