31ce62d485b3113aea7011cd8d60ba38196a6236
Mark Petrie <petrie.mark@gmail.com>
2017-07-10 21:34:08 -0700

    Add .eslintrc, .gitignore, .travis.yml and package.json files. Complete forEach function and passing forEach mocha test.

--- src/foreach.js
+++ src/foreach.js
@@ -1,14 +1,12 @@
 
 function forEach(array, callback) {
 
-for (i=0; i < array.length; i++) {
-    var item = array[i];
-    var index = i;
-    
-    callback(item, index);
+    for (i = 0; i < array.length; i++) {
+        var item = array[i];
+        var index = i;
 
+        callback(item, index);
     }
-
 }
 
 module.exports = forEach;
--- tests/foreach.test.js
+++ tests/foreach.test.js
@@ -4,16 +4,15 @@ const assert = require('assert');
 
 describe('forEach', () => {
 
-    it('Adds 1 to each item in the array', () => {
-            const array = [1, 2, 3];
-            const copy = [];
-            var callback = (item, index) => {
-                copy[index] = item;
-            }
-            forEach(array, callback);
-            assert.deepEqual(copy, array);
-
+    it('creates a copy of array', () => {
+        const array = [1, 2, 3];
+        const copy = [];
+        var callback = (item, index) => {
+            copy[index] = item;
+        };
+        
+        forEach(array, callback);
+        assert.deepEqual(copy, array);
     });
-
 });
 
