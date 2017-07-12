6e797834e391dc0a4ecd11d5ba8eb80220d05e1b
christylaguardia <christinelaguardia@gmail.com>
2017-07-11 21:25:37 -0700

    updated foreach to include callback

--- src/array-methods.js
+++ src/array-methods.js
@@ -1,45 +1,39 @@
-const methods = {};
-// push
-methods.push = function(array, item) {
-    array.push(item);
-    return array.length;
-};
-// for each
-methods.foreach = function(array) {
-    const numbers = [];
-    for (var i = 0; i < array.length; i++) {
-        // console.log(Number.isInteger(array[i]));
-        if (Number.isInteger(array[i])) {
-            numbers.push(array[i]);
+module.exports = {
+    foreach(array, callback) {
+        for (let i = 0; i < array.length; i++) {
+            callback(array[i], i);
         }
     }
-    // console.log('count of numbers in array', numbers.length);
-    return numbers.length;
 };
 
-// map
-methods.map = function(array) {
-    const newArray = [];
-    for (var i = 0; i < array.length; i++) {
-        newArray[i] = array[i];
-    }
-    // console.log(newArray);
-    return newArray.length;
-};
-// filter
-methods.filter = function(array) {
-    const filteredArray = array.filter(function(item) {
-        return Number.isInteger(item);
-    });
-    return filteredArray.length;
-};
-// reduce
-// methods.f
-// find index
-// methods.reduce = function(array, item) {
-//     return array.findIndex(item); 
+// // map
+// methods.map = function(array) {
+//     const newArray = [];
+//     for (var i = 0; i < array.length; i++) {
+//         newArray[i] = array[i];
+//     }
+//     // console.log(newArray);
+//     return newArray.length;
 // };
+// // filter
+// methods.filter = function(array, item) {
+//     // const filteredArray = array.filter(function(item) {
+//     //     return Number.isInteger(item);
+//     // });
+//     const filteredArray = [];
+//     for (var i = 0; i < array.length; i++) {
+//         if (array[i] == item) {
+//             // filteredArray
+//         }
+//     }
+//     return filteredArray.length;
+// };
+// // reduce
+// // methods.f
+// // find index
+// // methods.reduce = function(array, item) {
+// //     return array.findIndex(item); 
+// // };
 
-// every
+// // every
 
-module.exports = methods;
--- tests/array-methods.test.js
+++ tests/array-methods.test.js
@@ -2,31 +2,28 @@ const assert = require('assert');
 const methods = require('../src/array-methods');
 
 describe('array library', () => {
-    // push
-    it('adds a value to the array then returns the length', () => {
-        const array = [1, 2, 3];
-        const length = methods.push(array, 4);
-        assert.equal(length, 4);
-        assert.equal(array[3], 4);
-    });
     // for each
     it('loops through the array and counts numbers', () => {
-        const array = [1, 'a', 2, 'b', 3, 'c'];
-        const length = methods.foreach(array);
-        assert.equal(length, 3);
+        const array = [1, 2, 3];
+        const newArray = [];
+        methods.foreach(array, x => newArray.push(x));
+        assert.deepEqual(newArray, array);
     });
     // map
-    it('maps the array to a new array and returns the new length', () => {
-        const array = [1, 2, 3];
-        const length = methods.map(array);
-        assert.equal(length, 3);
-    });
-    // filter
-    it('filters the array for numbers and returns the new length', () => {
-        const array = [1, 'a', 2, 'b'];
-        const index = methods.filter(array);
-        assert.equal(index, 2);
-    });
+
+
+
+    // it('maps the array to a new array and returns the new length', () => {
+    //     const array = [1, 2, 3];
+    //     const length = methods.map(array);
+    //     assert.equal(length, 3);
+    // });
+    // // filter
+    // it('filters the array for numbers and returns the new length', () => {
+    //     const array = [1, 'a', 2, 'b'];
+    //     const index = methods.filter(array);
+    //     assert.equal(index, 2);
+    // });
     // reduce
     // it('reduces the array and returns sum', () => {
     //     const array = [1, 2, 3];
